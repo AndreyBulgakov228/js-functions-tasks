a32c7f40884ba155f193bebc01e331098ad24325
AndreyBul <andryushenka_bulgakov@list.ru>
2025-04-23 17:31:35 +0300

    laba4

--- __tests__/4-first-class-citizen.tests.js
+++
@@ -1,9 +0,0 @@
-import run from '../solutions/4-first-class-citizen.js';
-
-test('Проверка функции run()', () => {
-  expect(run('')).toBeNull();
-  expect(run('cb')).toBeNull();
-  expect(run('power')).toBe('rewo');
-  expect(run('kids')).toBe('sdik');
-  expect(run('hexlet')).toBe('telx');
-});
